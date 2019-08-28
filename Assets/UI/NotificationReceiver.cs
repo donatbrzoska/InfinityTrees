37e5b165c67ed7af75b726416ce84789725d819e
Donat Deva Brzoska <s0553655@htw-berlin.de>
2019-08-28 15:34:02 +0200

    renamed MessageReceiver

--- Assets/UI/NotificationReceiver.cs
+++ Assets/UI/NotificationReceiver.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MessageReceiver : MonoBehaviour
+public class NotificationReceiver : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
